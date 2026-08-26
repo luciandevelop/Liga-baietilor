01f5422c885d92e8abea3c9f0d8c30b8032a6142
luciandevelop <288139010+luciandevelop@users.noreply.github.com>
2026-08-26 17:50:45 +0000

    chore: actualizeaza manifestul de avataruri

--- src/assets/avatarManifest.generated.js
+++ src/assets/avatarManifest.generated.js
@@ -15,6 +15,20 @@ export default {
       "5": "5.png"
     }
   },
+  "bogdanb": {
+    "nextIndex": 1,
+    "files": {}
+  },
+  "doctoru": {
+    "nextIndex": 6,
+    "files": {
+      "1": "file_000000003b7881f6b6e87baead39caa5.png",
+      "2": "file_000000008e18822f9b3fe4547522c0cb.png",
+      "3": "file_0000000096788211afd3cc462ca9f728.png",
+      "4": "file_00000000ed20820c9409d1b521c005df.png",
+      "5": "file_00000000efd081f4815791032d0edf8a.png"
+    }
+  },
   "luck87": {
     "nextIndex": 6,
     "files": {

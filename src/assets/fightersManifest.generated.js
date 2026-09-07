5790ed19c3be908b25079f72004866e649709d6c
luciandevelop <288139010+luciandevelop@users.noreply.github.com>
2026-09-07 10:37:58 +0000

    chore: actualizeaza manifestul de personaje de lupta

--- src/assets/fightersManifest.generated.js
+++ src/assets/fightersManifest.generated.js
@@ -47,6 +47,12 @@ export default {
         "1": "file_0000000023e081f4aa7adda15cf39939.png"
       }
     },
+    "ghioca": {
+      "nextIndex": 2,
+      "files": {
+        "1": "ghioca_300x420.webp"
+      }
+    },
     "luck87": {
       "nextIndex": 2,
       "files": {
